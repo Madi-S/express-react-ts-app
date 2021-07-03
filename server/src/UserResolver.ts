1f634949a696a7bc4bd747be21704759e286c2b0
Madi-S <khovansky99@gmail.com>
2021-07-03 15:30:00 +0600

    Cookie and tokenization implemented

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,6 +1,23 @@
-import { Arg, Mutation, Query, Resolver } from 'type-graphql'
-import { hash } from 'bcryptjs'
+import {
+    Arg,
+    Mutation,
+    Query,
+    Resolver,
+    Field,
+    ObjectType,
+    Ctx,
+    UseMiddleware
+} from 'type-graphql'
+import { hash, compare } from 'bcryptjs'
 import { User } from './entity/User'
+import { MyContext } from './MyContext'
+import { createAccessToken, createRefreshToken } from './auth'
+
+@ObjectType()
+class LoginResponse {
+    @Field()
+    accessToken: string
+}
 
 @Resolver()
 export class UserResolver {
@@ -9,9 +26,42 @@ export class UserResolver {
         return 'Hi!'
     }
 
+    @Query(() => String)
+    @UseMiddleware()
+    bye() {
+        return 'Bye!'
+    }
+
     @Query(() => [User])
     users() {
         return User.find()
+    }
+
+    @Mutation(() => LoginResponse)
+    async login(
+        @Arg('email', () => String) email: string,
+        @Arg('password', () => String) password: string,
+        @Ctx() { res }: MyContext
+    ): Promise<LoginResponse> {
+        const user = await User.findOne({ where: { email } })
+        if (!user) {
+            throw new Error('User does not exist with given email')
+        }
+
+        const valid = await compare(password, user.password)
+        if (!valid) {
+            throw new Error('Incorrect credentials')
+        }
+
+        // succesfful login -> provide token
+
+        res.cookie('jid', createRefreshToken(user), {
+            httpOnly: true
+        })
+
+        return {
+            accessToken: createAccessToken(user)
+        }
     }
 
     @Mutation(() => Boolean)
