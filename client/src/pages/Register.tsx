624996be627a2d2f5bf3aee9d7b4f12669228d55
Madi-S <khovansky99@gmail.com>
2021-07-04 13:32:53 +0600

    React registration implemented

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+import { useRegisterMutation } from '../generated/graphql'
 
-interface Props {}
 
-const Register: React.FC<Props> = () => {
-    return <div className='reigster'>Register Page</div>
+const Register: React.FC<RouteComponentProps> = ({history}) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [register] = useRegisterMutation()
+    const onSubmit = async (e: { preventDefault: () => void }) => {
+        e.preventDefault()
+        console.log('Form submitted', email, password)
+
+        const response = await register({ variables: { email, password } })
+        console.log(response)
+        if (response.data && response.data.register) {
+            history.push('/')
+        }
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <input
+                    type='text'
+                    placeholder='test@test.com'
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    required
+                />
+                <input
+                    type='password'
+                    placeholder='********'
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                />
+            </div>
+            <button type='submit'>Register</button>
+        </form>
+    )
 }
 
 export default Register
